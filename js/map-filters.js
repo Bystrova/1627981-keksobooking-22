dba5832af94a704c99557e4b4b9cad4ff4a9a445
Александра <antonenko.a.b@gmail.com>
2021-03-10 15:32:12 +0300

    Замена сортировки на фильтрацию

--- js/map-filters.js
+++ js/map-filters.js
@@ -8,46 +8,37 @@ const housingRooms = document.querySelector('#housing-rooms');
 const housingGuests = document.querySelector('#housing-guests');
 const SIMILAR_ANNOUNCEMENT_COUNT = 10;
 const MAKE_MARKERS_DELAY = 500;
+const prices = {low: {min: 0, max: 10000}, middle: {min: 10000, max: 50000}, high: {min: 50000, max: 100500000000}};
 
-const getRank = (announcement) => {
-  const prices = {low: {max: 10000}, middle: {min: 10000, max: 50000}, high: {min: 50000}, any: {min: 0}};
-  const priceInterval = prices[housingPrice.value];
-  let rank = 0;
-  if(announcement.offer.type === housingType.value){
-    rank += 5;
-  }
-  if(announcement.offer.price >= priceInterval.min && announcement.offer.price <= priceInterval.max) {
-    rank += 4;
-  }
-  if(announcement.offer.rooms.toString() === housingRooms.value) {
-    rank += 3;
-  }
-  if(announcement.offer.guests.toString() === housingGuests.value) {
-    rank += 2;
-  }
+const isChoosen = (announcement) => {
   const checkedCheckboxes = Array.from(document.querySelectorAll('.map__checkbox:checked'));
-  checkedCheckboxes.forEach((checkbox) => {
-    if (announcement.offer.features.includes(checkbox.value, 0)) {
-      rank += 1;
-    }
-  });
-  return rank;
-};
-
-const sortAnnouncements = (announcementA, announcementB) => {
-  const rankA = getRank(announcementA);
-  const rankB = getRank(announcementB);
-  return rankB - rankA;
+  const priceInterval = prices[housingPrice.value];
+  let isFiltered = true;
+  if(housingType.value !== 'any' && announcement.offer.type !== housingType.value){
+    isFiltered = false;
+  } else if(housingPrice.value !== 'any' && !(announcement.offer.price >= priceInterval.min && announcement.offer.price <= priceInterval.max)){
+    isFiltered = false;
+  } else if(housingRooms.value !== 'any' && announcement.offer.rooms.toString() !== housingRooms.value){
+    isFiltered = false;
+  } else if(housingGuests.value !== 'any' && announcement.offer.guests.toString() !== housingGuests.value){
+    isFiltered = false;
+  } else {
+    checkedCheckboxes.forEach((checkbox) => {
+      if (!announcement.offer.features.includes(checkbox.value, 0)) {
+        isFiltered = false;
+      }
+    })
+  }
+  return isFiltered
 };
 
 const makeMarkersDebounce = _.debounce((evt) => {
   if (evt.target.classList.contains('map__filter') || evt.target.classList.contains('map__checkbox')){
-    const sortedArray = announcementsArray
-      .slice()
-      .sort(sortAnnouncements)
+    const filteredArray = announcementsArray.filter(announcement => isChoosen(announcement));
+    filteredArray
       .slice(0, SIMILAR_ANNOUNCEMENT_COUNT);
 
-    makeMarkers(sortedArray);
+    makeMarkers(filteredArray);
   }
 }, MAKE_MARKERS_DELAY);
 
